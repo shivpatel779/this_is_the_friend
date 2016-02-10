02b823f9ea6babe62a78c0e9deb80ec86d62e54f
Toby <you@example.com>
2016-02-10 10:54:42 -0800

    added friendship model and associations with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+	has_many :friendships
+	has_many :received_friendships, class_name: "Friendship", foreign_key: "friend_id"
+
+	has_many :active_friends, -> { where(friendships: { accepted: true}) }, through: :friendships, source: :friend
+	has_many :received_friends, -> { where(friendships: { accepted: true}) }, through: :received_friendships, source: :user
+	has_many :pending_friends, -> { where(friendships: { accepted: false}) }, through: :friendships, source: :friend
+	has_many :requested_friendships, -> { where(friendships: { accepted: false}) }, through: :received_friendships, source: :user
+
+	def friends
+	  active_friends | received_friends
+	end
+
+	def pending
+		pending_friends | requested_friendships
+	end
+
+	def name
+		email.split('@')[0].titleize
+	end
+
 end
