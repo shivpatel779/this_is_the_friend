8082e91700b3d91f6ef1f730bec787ae120bd0d5
Toby <you@example.com>
2016-02-10 09:57:28 -0800

    got devise and a root route

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 User.create!([
-  {name: "John Doe",    email: "john@example.com",    password: "password1", password_confirmation: "password1"},
-  {name: "Jane Doe",    email: "jane@example.com",    password: "password1", password_confirmation: "password1"},
-  {name: "Jeff Doe",    email: "jeff@example.com",    password: "password1", password_confirmation: "password1"},
-  {name: "Jack Doe",    email: "jack@example.com",    password: "password1", password_confirmation: "password1"},
-  {name: "Jenny Doe",   email: "jenny@example.com",   password: "password1", password_confirmation: "password1"},
-  {name: "Ginny Doe",   email: "ginny@example.com",   password: "password1", password_confirmation: "password1"},
-  {name: "Jared Doe",   email: "jared@example.com",   password: "password1", password_confirmation: "password1"},
-  {name: "Giles Doe",   email: "giles@example.com",   password: "password1", password_confirmation: "password1"},
-  {name: "Gollum Doe",  email: "gollum@example.com",  password: "password1", password_confirmation: "password1"}
+  {email: "john@example.com",   password: "password1", password_confirmation: "password1"},
+  {email: "jane@example.com",   password: "password1", password_confirmation: "password1"},
+  {email: "jeff@example.com",   password: "password1", password_confirmation: "password1"},
+  {email: "jack@example.com",   password: "password1", password_confirmation: "password1"},
+  {email: "jenny@example.com",  password: "password1", password_confirmation: "password1"},
+  {email: "ginny@example.com",  password: "password1", password_confirmation: "password1"},
+  {email: "jared@example.com",  password: "password1", password_confirmation: "password1"},
+  {email: "giles@example.com",  password: "password1", password_confirmation: "password1"},
+  {email: "gollum@example.com", password: "password1", password_confirmation: "password1"}
 ])
